365d4f9434dab256450e7e85954851bbe3f3acdd
AnatoliyGIT <kovrigin.anatoliy@gmail.com>
2020-04-30 17:52:37 +0300

    version new

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,11 +2,8 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
