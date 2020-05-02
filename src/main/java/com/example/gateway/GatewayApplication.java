a35ba8a25cc2ad06eec41357ea2e92215ba33437
Anatoly <kovrigin.anatoliy@gmail.com>
2020-05-02 17:54:48 +0300

    v40

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,19 +1,19 @@
 package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
 @EnableZuulProxy
-@EnableEurekaClient
-@SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
